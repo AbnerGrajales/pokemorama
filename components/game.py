a368420e9fd273fd89c0d0cb7d19d639f677ac4b
Abner Grajales <abner@sunwise.io>
2023-08-24 15:45:43 -0600

    feat: initial

--- components/game.py
+++ components/game.py
@@ -1,4 +1,7 @@
 import pygame
+import random
+
+from .core import Card
 
 # Inicializar pygame
 pygame.init()
@@ -6,20 +9,46 @@ pygame.init()
 # Configuración de la ventana
 WIDTH, HEIGHT = 800, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("¡Hola, Mundo!")
+pygame.display.set_caption("PokeMemorama")
 
 # Colores
 WHITE = (255, 255, 255)
+BGWIN = (249, 102, 200)
+
+# Variables para la cuadrícula de cartas
+ROWS, COLS = 4, 4
+GAP = 10
+CARD_WIDTH = (WIDTH - (COLS + 1) * GAP) // COLS
+CARD_HEIGHT = (HEIGHT - (ROWS + 1) * GAP) // ROWS
+
 
 # Clase principal del juego     
 class Pokememorama:
     def __init__(self):
-        self.cards = []
+        self.cards = self.load_cards()
         self.flipped_cards = []
         self.matches = 0
 
     def load_cards(self):
-        pass
+        # Cargamos las imagenes
+        images = self.load_images()
+        # Duplicamos las imagenes
+        images *=2
+        
+        # Hacer Random las imagenes
+        random.shuffle(images)
+        cards = []
+        for row in range(ROWS):
+            for col in range(COLS):
+                index = len(cards)
+                x = col * (CARD_WIDTH + GAP) + GAP
+                y = row * (CARD_HEIGHT + GAP) + GAP
+                rect = pygame.Rect(x, y, CARD_WIDTH, CARD_HEIGHT)
+                image = images[index]
+                card = Card(image, rect)
+                cards.append(card)
+                
+        return cards
     
     def load_images(self):
         pictures = [
@@ -35,7 +64,7 @@ class Pokememorama:
         images = []
         for i, imname in enumerate(pictures):
             picture = pygame.image.load(f"./images/{imname}")
-            # picture = pygame.transform.scale(picture, (WIDTH, HEIGHT))
+            picture = pygame.transform.scale(picture, (CARD_WIDTH, CARD_HEIGHT))
             images.append(picture)
 
         return images
@@ -54,13 +83,13 @@ class Pokememorama:
                 # Eventos
                 if event.type == pygame.QUIT:
                     running = False
-
-            WIN.fill(WHITE)  # Rellenar la pantalla con el color blanco
             
-            # Cargar la imagen en la pantalla
-            first_image = self.load_images()[0]
-            image_rect = first_image.get_rect(center=(WIDTH // 2, HEIGHT // 2))
-            WIN.blit(first_image, image_rect)
+            # Rellenar la pantalla de color
+            WIN.fill(BGWIN)
+            
+            # Dibujamos la cuadricula
+            for card in self.cards:
+                WIN.blit(card.image, card.rect.topleft)
                 
             pygame.display.update()
             clock.tick(30)
